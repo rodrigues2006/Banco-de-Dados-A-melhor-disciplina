764f759fff8f037e4b055da8e9a69568606409f9
Joao Victor <joaovictor.zuca12@gmail.com>
2023-09-07 22:45:58 -0300

    atividade 2 e 3 feita

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,1 +1,5 @@
 1- SELECT titulo FROM livros;
+
+2-SELECT nome FROM autores WHERE nascimento <= '1900-01-01';
+
+3-SELECT livros.titulo FROM livros JOIN autores ON livros.autor_id = autores.id WHERE autores.nome = 'J.K. Rowling';

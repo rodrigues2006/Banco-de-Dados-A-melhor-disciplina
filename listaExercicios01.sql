4a37e3ec38c1893af7cc96e97df6df5172ef120b
Joao Victor <joaovictor.zuca12@gmail.com>
2023-09-07 22:47:30 -0300

    atividade 4 feita

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -3,3 +3,5 @@
 2-SELECT nome FROM autores WHERE nascimento <= '1900-01-01';
 
 3-SELECT livros.titulo FROM livros JOIN autores ON livros.autor_id = autores.id WHERE autores.nome = 'J.K. Rowling';
+
+4- select nome, curso from alunos inner join matriculas on alunos.id = matriculas.aluno_id where curso = 'Engenharia de Software';

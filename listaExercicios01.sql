703932a3dc55a9f14ad6fa4b04f07f55e01844aa
Joao Victor <joaovictor.zuca12@gmail.com>
2023-09-07 23:14:28 -0300

    atividade 5 feito

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -5,3 +5,5 @@
 3-SELECT livros.titulo FROM livros JOIN autores ON livros.autor_id = autores.id WHERE autores.nome = 'J.K. Rowling';
 
 4- select nome, curso from alunos inner join matriculas on alunos.id = matriculas.aluno_id where curso = 'Engenharia de Software';
+
+5- select produto, Sum(receita) from vendas group by produto;

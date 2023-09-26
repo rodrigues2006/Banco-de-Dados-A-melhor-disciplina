a8ae210f937f44d9d2676148fef7ca145a318bce
Joao Victor <joaovictor.zuca12@gmail.com>
2023-09-25 23:09:00 -0300

    Atividade 2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -9,3 +9,19 @@ end;
 //
 
 call sp_ListarAutores ();
+
+
+
+2- create procedure sp_LivrosPorCategoria (in NomeCategoria varchar(100))
+
+begin
+
+declare ID_categoria int;
+select Categoria_ID into ID_categoria from categoria where NomeCategoria = nome;
+select * from livro where Categoria_ID = ID_categoria;
+
+end;
+//
+
+call sp_LivrosPorCategoria  ('Romance');
+//

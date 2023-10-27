0b7e6cbe7a73b786af74f46a74d97b45cb8f1b89
Joao Victor <joaovictor.zuca12@gmail.com>
2023-10-26 21:15:01 -0300

    EX 2

--- exerciciosDeFixacao20231026.sql
+++ exerciciosDeFixacao20231026.sql
@@ -3,3 +3,11 @@ create trigger after_Cliente_insert
  after insert on Clientes
   FOR EACH row
   insert into Auditoria (mensagem, data_hora) values ('Novo Cliente', now());
+
+----2-----------------------
+ create trigger before_Cliente_delete
+  before delete on Clientes
+  for each row
+  insert into Auditoria
+  set action = 'Delete',
+  nome = old.nome;
